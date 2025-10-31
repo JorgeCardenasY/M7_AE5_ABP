2e157194a7b112999466af2cae1a4f400a83cac8
JorgeCardenasY <jorge.cardenas.y@gmail.com>
2025-10-31 12:40:44 -0300

    Saving the approach

--- mi_proyecto/mi_proyecto/urls.py
+++ mi_proyecto/mi_proyecto/urls.py
@@ -3,4 +3,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('', include('productos.urls')),
+    path('admin/', admin.site.urls),
 ]
--- mi_proyecto/productos/urls.py
+++ mi_proyecto/productos/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('productos/', views.productos_view, name='productos'),
 ]
